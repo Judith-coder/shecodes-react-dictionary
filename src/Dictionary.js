a6f411ac1b9700f3f317a68d8dced4d209be9f20
Judith-coder <80777018+Judith-coder@users.noreply.github.com>
2021-06-30 15:40:20 +0200

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
 let [word, setWord] = useState("");
 
-function handleSubmit(event){
+function handleApiResponse(response){
+    console.log(response.data[0])
+}
+
+function handleSubmitWord(event){
     event.preventDefault();
-    alert(`Searching for ${word}`)
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`
+    axios.get(apiUrl).then(handleApiResponse)
+
+    // API documentation: https://dictionaryapi.dev/
 }
 
 function handleWordUpdate(event){
@@ -17,7 +25,7 @@ function handleWordUpdate(event){
     return (
 <div className="Dictionary">
     <h2>What word are you looking for ? </h2>
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmitWord}>
         <input type="search" placeholder="Name your word" autoFocus={true} onChange={handleWordUpdate} />
         <input type="submit" value="Search 🧐🤔🔍" />
     </form>
